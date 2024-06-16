e0dce541f542b324ee1fd25f4be0057a89d1af6c
kaushikchitipothu <kaushikchitipothu5@gmail.com>
2024-06-16 19:03:13 -0400

    Initial commit

---
+++ RunGroupWebApp/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+        public  DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
